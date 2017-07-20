a3586d309f592e5a5e542a85071c2b3167b61fff
Anurag-545 <ranaasr60@gmail.com>
2017-07-20 23:15:39 +0530

    class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+export class Hero {
+id:number;
+name:string;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details! </h2>
+  <div><label>id:</label>{{hero.id}}</div>
+  <div>
+     <label>name:</label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  `
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Myfirst App';
+  title = 'Tour Of Heroes';
+  hero:Hero ={
+    id:1,
+    name:'Anurag'
+  };
 }
