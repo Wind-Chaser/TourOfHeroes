722a108fae1b476c64936af1ab923772a595c90a
Anurag-545 <ranaasr60@gmail.com>
2017-07-22 16:54:41 +0530

    Routing Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
+import {DashBoardComponent} from './dashboard.component';
 import {HeroService} from './hero.service';
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+       {
+         path:'',
+         redirectTo:'/dashboard',
+         pathMatch:'full'
+       },
+      {
+        path:'heroes',
+        component:HeroesComponent
+      },
+      {
+        path:'dashboard',
+        component:DashBoardComponent
+      },
+      {
+        path:'detail/:id',
+        component:HeroDetailComponent
+      }
+
+    ])
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,5 +5,6 @@ export const HEROES:Hero[]=[
   {id:12,name:'Nikhil'},
   {id:13,name:'Rahul'},
   {id:14,name:'Sahil'},
-  {id:15,name:'Gaurav'}
+  {id:15,name:'Gaurav'},
+  {id:16,name:'Amit'}
 ];
